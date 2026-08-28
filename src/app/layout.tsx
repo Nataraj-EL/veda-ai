8c2d3b195c29e42577cd15796071781ebb9ea401
Nataraj EL <natarajel.dev@gmail.com>
2026-08-28 12:36:07 +0530

    feat(ui): add iOS-style bottom gesture line indicator for all mobile viewports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${bricolage.variable} h-full`}>
-      <body className="min-h-full flex flex-col font-sans antialiased">
+      <body className="min-h-full flex flex-col font-sans antialiased relative">
         <ClickBurstProvider>{children}</ClickBurstProvider>
+        {/* iOS-style bottom gesture line indicator for mobile layouts */}
+        <div className="fixed bottom-2 left-1/2 -translate-x-1/2 z-50 w-[134px] h-[5px] rounded-full bg-[#9c9c9c] pointer-events-none md:hidden" />
       </body>
     </html>
   );
